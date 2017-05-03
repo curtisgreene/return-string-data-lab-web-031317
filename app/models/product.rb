185a1c37f9a592fdfd09d69b779756e2cdfd427e
curtisgreene <curtis.j.w.greene@gmail.com>
2017-05-03 16:12:43 -0400

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,21 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def in_stock
+    if inventory != nil && inventory > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def availability
+    if inventory != nil && inventory > 0
+      return "Available"
+    else
+      return "Sold Out"
+    end
+  end
+
 end
